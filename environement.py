f15a8129dfc49a03c499da505610e8b5f8b76950
Chloe Maurel <maurel.chloe@gmail.com>
2023-06-09 12:44:41 +0800

    ajout de parametres pour l'env

--- environement.py
+++ environement.py
@@ -5,16 +5,23 @@ import numpy as np
 class LoanEnvironment(gym.Env):
     def __init__(self):
         # Define the action space and observation space
-        self.action_space = spaces.Box(low=0, high=np.inf, shape=(1,), dtype=np.float32)
+        self.action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32)
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(6,), dtype=np.float32)
 
         # Define any other necessary variables for the environment
         self.current_step = 0
-        self.max_steps = 10
-        self.T = 10  # Duration of the loan
+        self.max_steps = 2
+        self.T = 2  # Duration of the loan in days
         self.L = 10000  # Loan amount
+        self.collateral_qty = 1000
+        self.collateral_value = 10
+        self.reusable_collateral = 0.5
+        self.interest_market = 0.08
+        self.interest_rehy = 0.03
+        self.equity_ratio = 5000/10000
 
         # Define other parameters specific to your problem
+
 
     def reset(self):
         # Reset the environment to the initial state
@@ -27,17 +34,44 @@ class LoanEnvironment(gym.Env):
     def step(self, action):
         # Execute an action in the environment and return the next state, reward, and done flag
 
+        #if collateral_qty * collateral_value * reusable_collateral
+
+        # Retrieve current state observation
+        observation = self._get_observation()
+
+        # Extract state variables from the observation
+        current_step, T, L, X, P0x, P1x = observation
+
         # Update the state based on the action and current step
+        X_new = action  # Amount of asset bought/sold at this step
+
+        # Calculate new asset value based on the loan and asset prices
+        value_t0 = X * P0x
+        value_t1 = X_new * P1x
+
+        # Update the state variables
+        self.current_step += 1
+        X = X_new
+
+        # Calculate the profit or loss made from the loan investment
+        profit_loss = value_t0 - value_t1
+
+        # Calculate the interest profit or loss implied by hypothecation
+        Tx = ...  # Interest rate in the market without hypothecation
+        Tx_prime = ...  # Interest from the lender with hypothecation
+        interest_profit_loss = Tx * value_t0 - Tx_prime * value_t0
 
         # Calculate the reward based on the simplified payoff formula
-
-        # Update the current step
+        reward = profit_loss + interest_profit_loss
 
         # Check if the episode is done
         done = self.current_step >= self.max_steps
 
+        # Update the state observation
+        observation = [self.current_step, T, L, X, P0x, P1x]
+
         # Return the next state, reward, done flag, and additional information if needed
-        return self._get_observation(), reward, done, {}
+        return np.array(observation, dtype=np.float32), reward, done, {}
 
     def _get_observation(self):
         # Return the current state as an observation
